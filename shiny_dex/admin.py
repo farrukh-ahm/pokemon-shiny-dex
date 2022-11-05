2f7e75d4e452aa08dc4b3cf3b607d0a06eade197
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2022-11-05 13:14:08 +0000

    Add slug fields. Register models to admin.

--- shiny_dex/admin.py
+++ shiny_dex/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Pokeball, Pokemon, Game, User_Shiny
 
 # Register your models here.
+@admin.register(Pokeball)
+class PokeballAdmin(admin.ModelAdmin):
+    list_display = ['pokeball_type']
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['game_version']
+
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['dex_no', 'slug', 'name','type_1', 'type_2', 'gen']
+    list_filter = ['type_1', 'type_2', 'gen']
+    search_fields = ['dex_no', 'name']
+
+
+@admin.register(User_Shiny)
+class UserShinyAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('user', 'pokemon')}
+    list_display = ['user', 'slug', 'pokemon', 'nick_name', 'game', 'pokeball',
+                            'gender', 'encounters', 'caught_on', 'method']
+    list_filter = ['user']
+    search_fields = ['user']
--- shiny_dex/models.py
+++ shiny_dex/models.py
@@ -11,21 +11,24 @@ class Game(models.Model):
     game_version = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
-        return self.game
+        return self.game_version
 
 
 class Pokeball(models.Model):
     pokeball_type = models.CharField(max_length=50, unique=True, default='pokeball')
 
     def __str__(self):
-        return self.pokeball
+        return self.pokeball_type
 
 
 class Pokemon(models.Model):
     dex_no = models.IntegerField(unique=True)
     name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True)
+    featured_image = CloudinaryField('image', default='placeholder')
     type_1 = models.CharField(max_length=50)
     type_2 = models.CharField(max_length=50)
+    gen = models.IntegerField(default=1)
 
     def __str__(self):
         return f'{self.dex_no}: {self.name}'
@@ -34,6 +37,8 @@ class Pokemon(models.Model):
 class User_Shiny(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='pokemon')
+    slug = models.SlugField(max_length=100, unique=True, null=True)
+    nick_name = models.CharField(max_length=50, null=True, unique=False)
     game = models.ForeignKey(Game, on_delete=models.PROTECT, related_name='game')
     pokeball = models.ForeignKey(Pokeball, on_delete=models.PROTECT, related_name='pokeball')
     gender = models.IntegerField(choices=GENDER, default=3)
