ea78cb3142521e48ac4b72e86f29f52c0be2c5e8
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2022-12-31 20:33:39 +0000

    Add allauth. Customise the login page

--- shiny_database/urls.py
+++ shiny_database/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', include('shiny_dex.urls'), name='dex_urls')
 ]
