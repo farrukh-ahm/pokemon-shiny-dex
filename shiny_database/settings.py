e8f06ef90df4d31237367d83ad7462b974fb6a84
farrukh-ahm <farrukh.adnan@yahoo.co.in>
2025-01-26 12:07:54 +0000

    change host

--- shiny_database/settings.py
+++ shiny_database/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shiny-dex.up.railway.app', "*"]
+ALLOWED_HOSTS = ['shiny-dex.up.railway.app']
 
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
